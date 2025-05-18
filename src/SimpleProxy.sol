4eded12c63c04bf799f02d11f4ca59acd2c9395b
oumaoumag <ouma.godwin10@gmail.com>
2025-05-19 00:45:23 +0300

    Add EIP-1967 implementation slot constant to SimpleProxy contract

--- src/SimpleProxy.sol
+++ src/SimpleProxy.sol
@@ -5,4 +5,10 @@ contract SimpleProxy {
     // Address of the contract owner
     address public owner;
 
+
+    // EIP-1967 storage slot for the implementation address
+    bytes32 private constant IMPLEMENTATION_SLOT = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
+
+    
+
 }
